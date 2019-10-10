570d9d416663cbfc8c5445ddda21005985dee516
Leo.Cai <1836630213@qq.com>
2019-10-10 10:57:13 +0800

    frm表单导出api新增代码

---
+++ src/main/java/com/fr/io/ExportApi_frm.java
@@ -0,0 +1,122 @@
+package com.fr.io;
+
+import com.fr.base.operator.common.CommonOperator;
+import com.fr.chart.activator.ChartBaseActivator;
+import com.fr.cluster.engine.activator.standalone.StandaloneModeActivator;
+import com.fr.config.activator.BaseDBActivator;
+import com.fr.config.activator.ConfigurationActivator;
+import com.fr.env.operator.CommonOperatorImpl;
+import com.fr.form.export.FormToWBExecutor;
+import com.fr.general.I18nResource;
+import com.fr.io.TemplateWorkBookIO;
+import com.fr.io.exporter.ImageExporter;
+import com.fr.io.exporter.PDFExporter;
+import com.fr.io.exporter.excel.stream.StreamExcel2007Exporter;
+import com.fr.main.impl.WorkBook;
+import com.fr.main.workbook.ResultWorkBook;
+import com.fr.module.Module;
+import com.fr.module.tool.ActivatorToolBox;
+import com.fr.report.ReportActivator;
+import com.fr.report.RestrictionActivator;
+import com.fr.report.module.ReportBaseActivator;
+import com.fr.report.write.WriteActivator;
+import com.fr.scheduler.SchedulerActivator;
+import com.fr.stable.WriteActor;
+import com.fr.store.StateServerActivator;
+import com.fr.workspace.simple.SimpleWork;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class ExportApi_frm { 
+    public static void main(String[] args) {
+        // 定义报表运行环境,用于执行报表
+        Module module = ActivatorToolBox.simpleLink(new BaseDBActivator(),
+                new ConfigurationActivator(),
+                new StandaloneModeActivator(),
+                new StateServerActivator(),
+                new SchedulerActivator(),
+                new ReportBaseActivator(),
+                new RestrictionActivator(),
+                new ReportActivator(),
+                new WriteActivator(),
+                new ChartBaseActivator());
+        SimpleWork.supply(CommonOperator.class, new CommonOperatorImpl());
+        String envpath = "C:\\Users\\hipsh\\Desktop\\apache-tomcat-8.5.38-10.0\\webapps\\webroot\\WEB-INF\\";//工程路径
+        SimpleWork.checkIn(envpath);
+        I18nResource.getInstance();
+        module.start();
+
+
+//        ResultWorkBook rworkbook = null;
+        try {
+            // 导出cpt，未执行模板工作薄
+
+//            WorkBook workbook = (WorkBook) TemplateWorkBookIO
+//                    .readTemplateWorkBook("测试模板.frm");
+            // 获取报表参数并设置值，导出内置数据集时数据集会根据参数值查询出结果从而转为内置数据集
+//            Parameter[] parameters = workbook.getParameters();
+//            parameters[0].setValue("华东");
+            // 定义parametermap用于执行报表，将执行后的结果工作薄保存为rworkBook
+            java.util.Map parameterMap = new java.util.HashMap();
+            parameterMap.put("aa", "1");
+//            for (int i = 0; i < parameters.length; i++) {
+//                parameterMap.put(parameters[i].getName(), parameters[i]
+//                        .getValue());
+//            }
+            //导出frm时用这个
+            ResultWorkBook re =  FormToWBExecutor.executeForm("test.frm",parameterMap);
+            // 定义输出流
+            FileOutputStream outputStream;
+//            // 将未执行模板工作薄导出为内置数据集模板
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//EmbExport.cpt"));
+//            EmbeddedTableDataExporter templateExporter = new EmbeddedTableDataExporter();
+//            templateExporter.export(outputStream, workbook);
+//            // 将模板工作薄导出模板文件，在导出前您可以编辑导入的模板工作薄，可参考报表调用章节
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//TmpExport.cpt"));
+//            ((WorkBook) workbook).export(outputStream);
+//            // 将结果工作薄导出为2003Excel文件
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//ExcelExport.xls"));
+//            ExcelExporter ExcelExport = new ExcelExporter();
+//            ExcelExport.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+            // 将结果工作薄导出为Excel文件
+            outputStream = new FileOutputStream(new File("C:\\FTPServer\\1528.xlsx"));
+            StreamExcel2007Exporter ExcelExport1 = new StreamExcel2007Exporter();
+            //ResultWorkBook re = FSFormletHandler.executeForm("calendar.frm",parameterMap);
+            
+            //ResultWorkBook re = FSFormletHandler.executeForm("test.frm",parameterMap);
+           // ExcelExport1.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+             ExcelExport1.export(outputStream,re);
+//            // 将结果工作薄导出为Word文件
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//WordExport.doc"));
+//            WordExporter WordExport = new WordExporter();
+//            WordExport.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+//            // 将结果工作薄导出为Pdf文件
+            outputStream = new FileOutputStream(new File("C:\\FTPServer\\PdfExport.pdf"));
+            PDFExporter PdfExport = new PDFExporter();
+            PdfExport.export(outputStream, re);
+//            // 将结果工作薄导出为Txt文件（txt文件本身不支持表格、图表等，被导出模板一般为明细表）
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//TxtExport.txt"));
+//            TextExporter TxtExport = new TextExporter();
+//            TxtExport.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+//            // 将结果工作薄导出为Csv文件
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//CsvExport.csv"));
+//            CSVExporter CsvExport = new CSVExporter();
+//            CsvExport.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+//            //将结果工作薄导出为SVG文件
+//            outputStream = new FileOutputStream(new File("/Users//susie//Downloads//SvgExport.svg"));
+//            SVGExporter SvgExport = new SVGExporter();
+//            SvgExport.export(outputStream, workbook.execute(parameterMap, new WriteActor()));
+//            //将结果工作薄导出为image文件
+            outputStream = new FileOutputStream(new File("C:\\FTPServer\\PngExport.png"));
+            ImageExporter ImageExport = new ImageExporter();
+            ImageExport.export(outputStream, re);
+            outputStream.close();
+            module.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            SimpleWork.checkOut();
+        }
+    }
+}
