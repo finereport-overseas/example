a92c12298e2314c1500d20b3efd91732743f8ad1
YuxinTang-rex <49036945+YuxinTang-rex@users.noreply.github.com>
2020-09-16 16:42:16 +0800

    Update LogApi.java

--- src/main/java/com/fr/log/LogApi.java
+++ src/main/java/com/fr/log/LogApi.java
@@ -1,12 +1,10 @@
 package com.fr.log;
 
-/**
- * 后台输出log信息-http://help.finereport.com/doc-view-746.html
- */
+
 public class LogApi {
     public static void main(String[] args) {
-        FineLoggerFactory.getLogger().info( "This is level info");    //需要服务器log级别为info时才会显示
-        FineLoggerFactory.getLogger().warn("This is level warning");   //需要服务器log级别为info、warning时才会显示
-        FineLoggerFactory.getLogger().error("This is level error");   //需要服务器log级别为info、warning、error时才会显示,10.0取消了server级别日志记录
+        FineLoggerFactory.getLogger().info( "This is level info");    //display when server log level is info
+        FineLoggerFactory.getLogger().warn("This is level warning");   //display when server log level is info,warning
+        FineLoggerFactory.getLogger().error("This is level error");   //display when server log level is info,warning,error,10.0 cancelled server level log
     }
 }
