f23d82e2936d033a39eb158a5d7dbbbc6a414b36
finereport-joe <66773527+finereport-joe@users.noreply.github.com>
2020-12-08 11:48:14 +0800

    StringImage

--- src/main/java/com/fr/function/StringImage.java
+++ src/main/java/com/fr/function/StringImage.java
@@ -1,4 +1,3 @@
-//图片在下文字在上
 package com.fr.function;
 
 import com.fr.base.GraphHelper;
@@ -14,7 +13,7 @@ import java.io.IOException;
 
 
 /**
- * 图片在下文字在上
+ * Text above image
  */
 public class StringImage extends AbstractFunction {
     @Override
